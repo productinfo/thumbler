c8d900c088ed1b2ef7df3ff3040bbac8e18e4e79
Shantanu Raj <s@sraj.me>
2018-03-14 23:32:57 +0530

    Add eslint closes #38
    Add prettier-eslint-cli

---
+++ toggl/eslint-config-toggl/README.md
@@ -0,0 +1,16 @@
+# eslint-config-toggl
+
+Reusable ESLit configuration.
+
+## Usage
+
+In `package.json`, specify:
+
+```
+"eslintConfig": {
+  "extends": "toggl"
+},
+"dependencies": {
+  "eslint-config-toggl": "1.0.0-next"
+}
+```
---
+++ toggl/eslint-config-toggl/src/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: ['node', 'promise'],
+  extends: [
+    'standard',
+    'plugin:node/recommended',
+    'plugin:promise/recommended'
+  ],
+  env: {
+    node: true
+  },
+  rules: {
+    // Built-in ESLint rules
+    'no-mixed-operators': 'off'
+  }
+}
