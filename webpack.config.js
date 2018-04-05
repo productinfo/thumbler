c65108da116e4da17663bed50216072e29542682
Shantanu Raj <s@sraj.me>
2018-04-06 00:40:57 +0530

    Bundle all node_modules to one file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const { join } = require('path')
-const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
@@ -9,22 +9,29 @@ const dir = name => join(__dirname, name)
 
 const outDir = name => join(outPath, name)
 
+// Extraneous consolidate dynamic requires
+const extraneousConsRequires = /(atpl|bracket-template|dot|dustjs-linkedin|ect|haml-coffee|hamlet|hamljs|hogan.js|htmling|jazz|jqtpl|just|liquor|marko|mote|mustache|plates|ractive|react|slm|teacup|templayed|toffee|twig|vash|velocityjs|walrus|whiskers)/
+
 module.exports = {
   mode: 'development',
   target: 'node',
   node: {
     __dirname: true
   },
-  externals: nodeExternals(),
-  entry: join(__dirname, 'bin', 'www'),
+  stats: { warnings: !false },
+  entry: join(__dirname, 'bin', 'www.js'),
   output: {
-    path: outPath,
-    filename: 'app.js'
+    path: outDir('bin'),
+    filename: 'www'
+  },
+  module: {
+    rules: [{ test: /bin\/coffee$/, use: 'shebang-loader' }]
   },
   resolve: {
     extensions: ['.web.js', '.mjs', '.js', '.json']
   },
   plugins: [
+    new webpack.IgnorePlugin(extraneousConsRequires),
     new CleanWebpackPlugin(outPath),
     new CopyWebpackPlugin([
       { from: dir('public'), to: outDir('public') },
